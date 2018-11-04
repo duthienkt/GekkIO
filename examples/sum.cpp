157ddec8c629b880389a6148660cfcb235aa9fa1
duthienkt <blueskythien2010@live.com>
2018-11-04 17:00:29 +0700

    + examples

---
+++ examples/sum.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <cstdlib>
+#include "gekkio.h"
+
+// be careful, without pack will make bug
+#pragma pack(1)
+struct Task
+{
+    int id;
+    int from;
+    int to;
+} ;
+
+struct Result{
+    int id;
+    long long sum;
+} ;
+#pragma pack()
+
+long long sum(int from, int to){
+    long long res = 0;
+    while(from<= to) {
+        res +=from;
+        ++from;
+    };
+    return res;
+}
+
+/**
+ *  test: [mess](Tmd1eWVuIFRoaSBUaHV5IEhhbmcA) 
+ *
+*/
+void onMessCallback(void *data, int size)
+{
+    Task *tsk = (Task *)data;
+    Result rslt;
+    rslt.id = tsk->id;
+    rslt.sum = sum(tsk->from, tsk->to);
+    gkio_send("result", &rslt, sizeof(Result));
+}
+
+int main()
+{
+    gkio_init(1024);
+    gkio_on("task", &onMessCallback);
+    int i = 0;
+    while (true)
+    {
+        gkio_loop();
+    }
+    return 0;
+}
---
+++ examples/sum.js
@@ -0,0 +1,71 @@
+const { GekkIO, GKIOLiteral, GKIOStruct } = require('../');
+const EventEmitter = require('events');
+
+var Task = GKIOStruct()
+    .property(GKIOLiteral.Int32, "id")
+    .property(GKIOLiteral.Int32, "a")
+    .property(GKIOLiteral.Int32, "b")
+    .declare();
+
+var Result = GKIOStruct()
+    .property(GKIOLiteral.Int32, "id")
+    .property(GKIOLiteral.Int64, "sum")
+    .declare();
+
+
+var SumWorkers = Array(4).fill(null).map(_ => new GekkIO('./sum'));
+var ResultEmitor = new EventEmitter();
+
+SumWorkers.forEach(worker => {
+    // worker.debug = true;
+    worker.on('result', data => {
+        var mResult = Result.fromBuffer(data);
+        ResultEmitor.emit('result_' + mResult.id.value, mResult.sum.value);
+    });
+});
+
+function sum0(a, b) {
+    return new Promise((rs, rj) => {
+        var res = 0;
+        while (a <= b) {
+            res += a;
+            ++a;
+        }
+        rs(res);
+    });
+}
+
+var uid = 0;
+function sum1(a, b) {
+    var d = (b - a + 1) >> 2;
+    var ranges = [[a, a + d], [a + d + 1, a + 2 * d], [a + 2 * d + 1, a + 3 * d], [a + 3 * d + 1, b]];
+    var tasks = ranges.map((range, i) => new Promise((rs, rj) => {
+        SumWorkers[i].send('task', Task({ id: uid, a: range[0], b: range[1] }).toBuffer());
+        ResultEmitor.once('result_' + uid, sum => rs(sum));
+        uid++;
+
+    }));
+
+    return Promise.all(tasks).then(results => {
+        return results[0] + results[1] + results[2] + results[3];
+    });
+}
+
+
+function Test(mess, func, a, b) {
+    var now = new Date().getTime();
+    return func(a, b).then(res => {
+        console.log(mess);
+        console.log("Time :", new Date().getTime() - now + '(ms)', "Result :", res);
+    })
+}
+
+console.log("Expected :", 500000000500000000);
+Test("Javascript", sum0, 0, 1000000000);
+Test("C++", sum1, 0, 1000000000).then(_ => {
+    SumWorkers.forEach(worker => worker.dispose());
+}
+);
+
+
+
